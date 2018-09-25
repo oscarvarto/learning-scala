eca146af016eeeea092b891e997f6480c113c548
Oscar Vargas Torres <oscarvarto@protonmail.com>
2018-09-25 11:57:53 -0500

    updated site

---
+++ js/mathjax-config.js
@@ -0,0 +1,8 @@
+MathJax.Hub.Config({
+    tex2jax: {
+        inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+        processEscapes: true
+    }
+});
+
+MathJax.Ajax.loadComplete("https://oscarvarto.github.io/learning-scala/js/mathjax-config.js");
